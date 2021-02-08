d8b570d003f56cea378f75fac6486e1933135f2d
aniket1004 <aniket1004@gmail.com>
2021-02-08 18:47:53 +0530

    Aniket Changed

--- database/db.php
+++ database/db.php
@@ -1,12 +1,14 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "swapnil123";
-    $dbname = "community";
+    $servername = "febinaevents.com";
+    $username = "febinaevents18_community_admin";
+    $password = "Febina@123";
+    $dbname = "febinaevents18_community";
+    $port = 3306;
 
     $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-    if (!$conn) {
+    if (!$conn) 
+    {
         die("Error : " . mysqli_connect_error());
-      }
+    }
 ?>
