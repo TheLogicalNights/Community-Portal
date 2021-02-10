7fbada132486cecbe00322834725432cfc911ac5
Kunal Singh <singhkunal2050@gmail.com>
2021-02-10 11:30:47 +0530

    added main section to members.php

--- members.php
+++ members.php
@@ -10,49 +10,52 @@
     $result = mysqli_query($conn,$query);
     $sno = 0;
 ?>
-<div class="jumbotron usp-section" >
-    <div class="container">
-        <center>
-            <h1>Members</h1>
-        </center>
-        <div class="cards-section">
-            <div class="container">
-                <div class="d-flex justify-content-start me-3">
-                    <input data-aos="fade-right" type="text" onkeyup="getSuggestion(this.value)" autocomplete="off" style="width: 45%;" class="form-control" name="search" placeholder="Enter name">
-                </div>
-                <div id="data">
-                    <table class='table mt-2' id='myTable'>
-                                    <thead>
+
+<main>
+    <div class="jumbotron usp-section" >
+        <div class="container">
+            <center>
+                <h1>Members</h1>
+            </center>
+            <div class="cards-section">
+                <div class="container">
+                    <div class="d-flex justify-content-start me-3">
+                        <input data-aos="fade-right" type="text" onkeyup="getSuggestion(this.value)" autocomplete="off" style="width: 45%;" class="form-control" name="search" placeholder="Enter name">
+                    </div>
+                    <div id="data">
+                        <table class='table mt-2' id='myTable'>
+                                        <thead>
+                                            <tr>
+                                                <th scope='col'>Member name</th>
+                                                <th scope='col'>Username</th>
+                                                <th scope='col'>Visit</th>
+                                            </tr>
+                                        </thead>
+                        <?php
+                                while($row = mysqli_fetch_assoc($result))
+                                {
+                                    $sno++;
+                        ?>
                                         <tr>
-                                            <th scope='col'>Member name</th>
-                                            <th scope='col'>Username</th>
-                                            <th scope='col'>Visit</th>
+                                            <td><?php echo $row['name']; ?></td>
+                                            <td><?php echo $row['username']; ?></td>
+                                            <td>
+                                                <form action='/Febina/Members-Portal/code' method='POST'>
+                                                    <input type='hidden' name='username' id='visit' value=<?php echo $row['username']; ?>>
+                                                    <button type='submit' name='VisitMember' class='visit btn btn-primary'>Visit</button>
+                                                </form>
+                                            </td>
                                         </tr>
-                                    </thead>
-                    <?php
-                            while($row = mysqli_fetch_assoc($result))
-                            {
-                                $sno++;
-                    ?>
-                                    <tr>
-                                        <td><?php echo $row['name']; ?></td>
-                                        <td><?php echo $row['username']; ?></td>
-                                        <td>
-                                            <form action='/Febina/Members-Portal/code' method='POST'>
-                                                <input type='hidden' name='username' id='visit' value=<?php echo $row['username']; ?>>
-                                                <button type='submit' name='VisitMember' class='visit btn btn-primary'>Visit</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                    <?php
-                            }
-                    ?>
-                    </table>
+                        <?php
+                                }
+                        ?>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</main>
     <script type="text/javascript">
         function getSuggestion(q)
         {
