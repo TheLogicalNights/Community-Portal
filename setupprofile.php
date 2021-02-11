98a9ec6359e45774b3495d9a44b136b858780949
TheLogicalNights <swapniladhav560@gmail.com>
2021-02-11 19:03:34 +0530

    Swapnil : Setup profile bugs has been fixed

--- setupprofile.php
+++ setupprofile.php
@@ -1,6 +1,6 @@
 <?php
-    include('header.php');
     session_start();
+    include('header.php');
     if(!isset($_SESSION['status']))
     {
         header("Location: /Febina/Members-Portal/signin");
